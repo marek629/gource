61e03d803f7821f95d193f1f450475c242371277
Marek Jędryka <marek629@poczta.onet.pl>
2024-03-31 20:42:50 +0200

    prettier added

--- gource.js
+++ gource.js
@@ -1,19 +1,24 @@
 #!/usr/bin/env zx
 
-import { readFile } from "fs/promises";
+import { readFile } from 'fs/promises'
 
 const { projects } = YAML.parse(
-  await readFile(path.join((await $`pwd`.quiet()).stdout.trim(), ".gourcezx.yml"), 'utf-8')
-);
+  await readFile(
+    path.join((await $`pwd`.quiet()).stdout.trim(), '.gourcezx.yml'),
+    'utf-8'
+  )
+)
 
 const args = projects.map(
   (p) => `"gource --output-custom-log - ${p} | sed -r 's#(.+)\\|#\\1|${p}#'"`
-);
+)
 
 const pipeline = $`yarn dlx concurrently --raw --max-processes 2 ${args}`
   .pipe($`grep -v '➤'`)
-  .pipe($`grep --color=never -E '^[0-9]+\|[[:alnum:][:space:]]+\|[AMD]+\|[[:alnum:][:space:][:punct:]]+$' -`)
+  .pipe(
+    $`grep --color=never -E '^[0-9]+\|[[:alnum:][:space:]]+\|[AMD]+\|[[:alnum:][:space:][:punct:]]+$' -`
+  )
   .pipe($`sort`.quiet())
-  .pipe($`gource --log-format custom -`.quiet());
+  .pipe($`gource --log-format custom -`.quiet())
 
 await spinner('gource is running', () => pipeline)
