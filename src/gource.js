a26b140da06cb48db92b9b8bdf2067ce606b4da2
Marek Jędryka <marek629@poczta.onet.pl>
2024-04-03 08:11:17 +0200

    add passing concurrently options from configuration file

--- src/gource.js
+++ src/gource.js
@@ -9,7 +9,7 @@ import 'zx/globals'
 import { args, cliOptions } from './args.js'
 
 const configFile = '.gourcezx.yml'
-const { projects, gource } = YAML.parse(
+const { projects, gource, concurrently } = YAML.parse(
   await readFile(join(cwd(), configFile), 'utf-8'),
 )
 const jobs = await args(projects, gource)
@@ -17,8 +17,12 @@ if (jobs.length < 1) {
   console.warn(`Nothing to do. Please check your config file: ${configFile}`)
   exit(1)
 }
+const concurrentlyOptions = cliOptions({
+  'max-processes': 2,
+  ...concurrently?.options,
+})
 
-const pipeline = $`yarn dlx concurrently --raw --max-processes 2 ${jobs}`
+const pipeline = $`yarn dlx concurrently --raw ${concurrentlyOptions} ${jobs}`
   .pipe($`grep -v '➤'`.quiet())
   .pipe(
     $`grep --color=never -E '^[0-9]+\|[[:alnum:][:space:]]+\|[AMD]+\|[[:alnum:][:space:][:punct:]]+$' -`,
